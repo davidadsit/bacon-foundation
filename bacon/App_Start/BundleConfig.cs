7d88da415d7e996c4764da79aa7b0f12de2c83f8
david-adsit <david-adsit@pluralsight.com>
2013-10-01 10:07:35 -0600

    Full Presentation

---
+++ bacon/App_Start/BundleConfig.cs
@@ -0,0 +1,28 @@
+﻿using System.Web.Optimization;
+
+namespace Bacon.App_Start
+{
+    public static class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            AddFoundationBundles(bundles);
+        }
+
+        static void AddFoundationBundles(BundleCollection bundles)
+        {
+             bundles.Add(new ScriptBundle("~/bundles/zepto").Include(
+                "~/Scripts/zepto.js"));
+
+            bundles.Add(new StyleBundle("~/Content/foundation/css").Include(
+                "~/Content/foundation/foundation.css",
+                "~/Content/foundation/foundation.mvc.css",
+                "~/Content/foundation/app.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/foundation").Include(
+                "~/Scripts/foundation/foundation.js",
+                "~/Scripts/foundation/foundation.*",
+                "~/Scripts/foundation/app.js"));
+        }
+    }
+}
--- bacon/Global.asax.cs
+++ bacon/Global.asax.cs
@@ -1,5 +1,7 @@
-﻿using System.Web.Http;
+﻿using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 using Bacon.App_Start;
 
@@ -7,7 +9,7 @@ namespace Bacon
 {
     // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
     // visit http://go.microsoft.com/?LinkId=9394801
-    public class MvcApplication : System.Web.HttpApplication
+    public class MvcApplication : HttpApplication
     {
         protected void Application_Start()
         {
@@ -16,6 +18,7 @@ namespace Bacon
             WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
